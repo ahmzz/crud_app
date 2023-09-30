271782b1c85a685d135144e0d106e680de5405fb
ahmzz <ahmedminhas247@outlook.com>
2023-09-30 17:57:02 +0500

    CRUD app project setup with middlewares

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const dotenv=require('dotenv')
+const path=require('path')
+const bodyParser=require('body-parser')
+const app=express()
+
+dotenv.config({
+    path:'config.env'
+})
+const PORT=process.env.PORT||8080
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.set("view engine","ejs")
+
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.use('/imgs',express.static(path.resolve(__dirname,'assets/imgs')))
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')))
+
+app.get('/',(req,res)=>{
+    res.json({message:"Hello"})
+})
+
+app.listen(PORT,()=>{
+    console.log("Server UP");
+})
