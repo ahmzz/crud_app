7ee52c0525c24ddd9d278c00f00daf9cdf35b1ad
ahmzz <ahmedminhas247@outlook.com>
2023-10-01 18:17:04 +0500

    Implemented create user API

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,6 +2,23 @@ const UserDB=require('../model/model')
 
 exports.create=(req,res)=>{
 
+    if(!req.body){
+        res.status(400).send({message:'Content cannot be empty'})
+        return 
+    }else{
+        const user=new UserDB({
+            name:req.body.name,
+            email:req.body.email,
+            gender:req.body.gender,
+            status:req.body.status
+        })
+
+        user.
+        save(user)
+        .then(data=>res.send(data))
+        .catch(err=>res.status(500).send({message:err.message||'Some error occured while creating user'}))
+    }
+
 }
 
 exports.find=(req,res)=>{
